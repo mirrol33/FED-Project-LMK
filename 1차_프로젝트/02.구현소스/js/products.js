b254b08187d0166f6e5bb6601af2d4215361301a
freelife11 <mirrol33@gmail.com>
2024-12-13 17:19:10 +0900

    12.13 업데이트1
    products.js 파일 추가, json 파일 로드후 출력 함수 추가

---
+++ 1차_프로젝트/02.구현소스/js/products.js
@@ -0,0 +1,31 @@
+// JSON 파일 불러오기
+async function loadProducts() {
+  const response = await fetch("./js/products.json"); // JSON 파일 경로
+  if (!response.ok) {
+    throw new Error(`Failed to fetch products.json: ${response.status}`);
+  }
+  return await response.json();
+}
+
+// 상품리스트 데이터 로드 및 출력
+loadProducts()
+  .then((products) => {
+    console.log("Loaded products:", products);
+
+    // 예: 데이터를 HTML에 렌더링
+    const proList = document.getElementById("product-list");
+    products.forEach((product) => {
+      const proBox = document.createElement("div");
+      proBox.className = "product-box";
+      proBox.innerHTML = `
+            <span>${product.name_en}</span>
+            <h3>${product.name}</h3>
+            <p>${product.description}</p>
+            <img src="${product.img}" alt="${product.name}">
+            `;
+      proList.appendChild(proBox);
+    });
+  })
+  .catch((error) => {
+    console.error("Error loading products:", error);
+  });
