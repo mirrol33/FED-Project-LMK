59711013ab784446180c59a8f4d0e95f77c767f2
freelife11 <mirrol33@gmail.com>
2024-12-20 16:45:55 +0900

    12.20 업데이트6

--- 1차_프로젝트/02.구현소스/js/products.js
+++ 1차_프로젝트/02.구현소스/js/products.js
@@ -25,7 +25,7 @@ $(() => {
     // Swiper 초기화
     let swiper = new Swiper(".mySwiper", {
       slidesPerView: 3,
-      spaceBetween: 40,
+      spaceBetween: 60,
       loop: true,
       loopedSlides: 2,
       centeredSlides: true,
@@ -53,7 +53,7 @@ $(() => {
         },
         1200: {
           slidesPerView: 2,
-          spaceBetween: 20,
+          spaceBetween: 40,
         },
       },
     });
@@ -61,21 +61,26 @@ $(() => {
     window.addEventListener("resize", swiperPosFn); // 리사이즈시 위치 호출 함수 실행
     // swiper 위치 호출 함수
     function swiperPosFn() {
-      let swiperTarget = swiper.getTranslate(); // swiper 슬라이드 위치 읽기
+      let swiperTranslate = swiper.getTranslate(); // 전체 슬라이드 위치 읽기
       let ww = window.innerWidth; // 윈도우 넓이 읽기
-      console.log("슬라이드 위치값:", swiperTarget);
+      console.log("슬라이드 위치값:", swiperTranslate);
       console.log("윈도우 넓이값:", ww);
-    }
+    };
 
     // 자세히 버튼 클릭시
     $(document).on("click", ".btn-view", function (e) {
       e.stopPropagation(); // 버블링 막기
       $(this).hide(); // 자세히보기 버튼 숨기기
       // 해당 슬라이드 열기
-      const $closestSlide = $(this).closest(".product-box");
-      $closestSlide.addClass("open").children(".close-btn").show();
+      const $closestSlide = $(this).closest(".product-box"); // 부모요소 대상
+      $closestSlide.addClass("open").children(".close-btn").show(); // .open 추가, 이전 버튼 표시
       // 해당 슬라이드 사이즈 변경 애니메이션
-      $closestSlide.animate({width: "1200px"}, 200);
+      $closestSlide.animate({width:"1200px"}, 200);
+      // 해당 슬라이드 위치변경
+      const $proPosition = $('.product-list'); // 슬라이드 위치변경 대상
+      $proPosition.animate({
+        transform: "translate3d("+swiperTranslate+"px, 0px, 0px)"
+      });
     });
 
     // 이전 버튼 클릭시
@@ -84,7 +89,16 @@ $(() => {
       $(this).hide(); // 이전 버튼 숨기기
       // 해당 슬라이드 닫기
       const $closestSlide = $(this).closest(".product-box");
-      $closestSlide.removeClass("open").children(".btn-view").show();
+      $closestSlide.removeClass("open").children(".btn-view").show(); // .open 삭제, 자세히 버튼 표시
     });
+
+    // 좌,우버튼 또는 페이지 버튼 클릭시 
+    // .swiper-button-prev, .swiper-button-next, .swiper-pagination span
+    $(document).on("click", ".swiper-button-prev,.swiper-button-next,.swiper-pagination span", function (e) {
+      console.log('좌우버튼,페이지버튼클릭시!');
+
+    });
+    
+
   }); // getJSON
 });
